4814aee4d859068f6c0d5dca4a617822dd72f5c4
clonedkmh <quocanh10a2@gmail.com>
2023-03-24 21:06:30 +0700

    change domain ip

--- lib/ultis/ApiEndpoint.dart
+++ lib/ultis/ApiEndpoint.dart
@@ -1,4 +1,5 @@
-const String DOMAIN = 'http://192.168.1.4:8080/api/v1/';
+// const String DOMAIN = 'http://192.168.1.4:8080/api/v1/';
+const String DOMAIN = 'http://192.168.137.1:8080/api/v1/';
 const String LOGIN_ENDPOINT = DOMAIN + 'login';
 const String COURSE_ENDPOINT = DOMAIN + 'courses';
 const String NEWEST_COURSE_ENDPOINT = DOMAIN + 'courses/newest';

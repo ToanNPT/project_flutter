c475a7a3afce56374e562aca446c2542188cfa75
QuocAnhDc <quocanh10a2@gmail.com>
2023-03-31 14:27:19 +0700

    fix search screen

--- lib/Screens/HomeScreens/Search.dart
+++ lib/Screens/HomeScreens/Search.dart
@@ -11,39 +11,105 @@ class Search extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Search Posts'),
+        backgroundColor: Colors.black,
+        title: Text('Search Courses'),
       ),
-      body: BlocProvider(
-        create: (context) => CoursesBloc(),
-        child: Column(
-          children: [
-            SearchBar(),
-            Expanded(
-              child: BlocBuilder<CoursesBloc, CourseState>(
-                builder: (context, state) {
-                  if (state is SearchLoadedState) {
-                    return ListView.builder(
-                      itemCount: state.courses.length,
-                      itemBuilder: (context, index) {
-                        final post = state.courses[index];
-                        return ListTile(
-                          title: Text(post.name),
-                        );
-                      },
-                    );
-                  } else if (state is SearchError) {
-                    return Center(
-                      child: Text(state.errorMessage),
-                    );
-                  } else {
-                    return Container();
-                  }
-                },
+      body: Container(
+        color: Colors.black,
+        child: BlocProvider(
+          create: (context) => CoursesBloc(),
+          child: Column(
+            children: [
+              SearchBar(),
+              Expanded(
+                child: BlocBuilder<CoursesBloc, CourseState>(
+                  builder: (context, state) {
+                    if (state is SearchLoadedState) {
+                      return ListView.builder(
+                        itemCount: state.courses.length,
+                        itemBuilder: (context, index) {
+                          final post = state.courses[index];
+                          return Container(
+                            padding: EdgeInsets.symmetric(vertical: 8.0),
+                            child: Row(
+                              children: [
+                                Container(
+                                  width: 100,
+                                  height: 100,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                  child: ClipRRect(
+                                    borderRadius: BorderRadius.circular(10),
+                                    child: FadeInImage.assetNetwork(
+                                      placeholder: (
+                                          "assets/images/udemy_logo_2.jpg"),
+                                      image:  post.avatar,
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
+                                ),
+                                SizedBox(width: 16),
+                                Expanded(
+                                  child: Container(
+                                    padding: EdgeInsets.all(16),
+                                    child: Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          post.name,
+                                          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold,color: Colors.white),
+                                        ),
+                                        SizedBox(height: 8),
+                                        Text(
+                                          post.description,
+                                          style: TextStyle(fontSize: 14, color: Colors.grey[600]),
+                                          maxLines: 1,
+                                          overflow: TextOverflow.ellipsis,
+                                        ),
+                                        SizedBox(height: 8),
+                                        Row(
+                                          children: [
+                                            Icon(Icons.star, color: Colors.yellow, size: 16),
+                                            Icon(Icons.star, color: Colors.yellow, size: 16),
+                                            Icon(Icons.star, color: Colors.yellow, size: 16),
+                                            Icon(Icons.star_border, color: Colors.yellow, size: 16),
+                                            Icon(Icons.star_border, color: Colors.yellow, size: 16),
+                                            SizedBox(width: 8),
+                                            Text(
+                                              '4.0',
+                                              style: TextStyle(fontSize: 14,color: Colors.white),
+                                            ),
+                                            SizedBox(width: 8),
+                                            Text(
+                                              post.price.toString(),
+                                              style: TextStyle(fontSize: 14,color: Colors.white),
+                                            ),
+                                          ],
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          );
+                        },
+                      );
+                    } else if (state is SearchError) {
+                      return Center(
+                        child: Text(state.errorMessage),
+                      );
+                    } else {
+                      return Container();
+                    }
+                  },
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
-      ),
+      )
     );
   }
 }
@@ -63,22 +129,42 @@ class _SearchBarState extends State<SearchBar> {
       child: Row(
         children: [
           Expanded(
-            child: TextField(
-              controller: _controller,
-              decoration: InputDecoration(
-                hintText: 'Search',
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border(
+                  bottom: BorderSide(
+                    color: Colors.white,
+                    width: 2.0,
+                  ),
+                ),
+              ),
+              child: TextField(
+                controller: _controller,
+                decoration: InputDecoration(
+                  hintText: 'Search',
+                ),
+                style: TextStyle(color: Colors.white),
               ),
             ),
+
           ),
           SizedBox(width: 8.0),
-          ElevatedButton(
-            child: Text('Search'),
+          ElevatedButton.icon(
+            icon: Icon(
+              Icons.search,
+              size: 32,
+              color: Colors.white,
+            ),
+            label: Text(''),
             onPressed: () {
               final query = _controller.text.trim();
               if (query.isNotEmpty) {
                 BlocProvider.of<CoursesBloc>(context).add(SearchCourseEvent(query));
               }
             },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.black,
+            ),
           ),
         ],
       ),
